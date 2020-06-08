e43371ab5985a9d45fe4b6657190b7ee6321770e
loopinlumin <38004530+loopinlumin@users.noreply.github.com>
2020-06-08 19:55:39 +0100

    id

--- src/constants/equipment-types.js
+++ src/constants/equipment-types.js
@@ -1,49 +1,144 @@
 const EQUIPMENT_TYPES = {
 	weapons: [
 		{
-			id: 1,
+			id: 1.1.1,
 			name: 'Bone Sword',
 			attack: 2,
 		},
 		{
-			id: 2,
+			id: ,
+			name: '',
+			attack: ,
+		},
+		{
+			id: ,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 1.2.1,
 			name: 'Dragon Hammer',
 			attack: 5,
 		},
 		{
-			id: 3,
+			id: ,
+			name: '',
+			attack: ,
+		},
+		{
+			id: ,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 1.3.1,
 			name: 'Royal Axe',
 			attack: 9,
+		},
+		{
+			id: ,
+			name: '',
+			attack: ,
+		},
+		{
+			id: ,
+			name: '',
+			attack: ,
 		},
 	],
 	armour: [
 		{
-			id: 4,
+			id: 2.1.1,
+			name: 'Helmet',
+			attack: ,
+		},
+		{
+			id: 2.1.2,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.1.3,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.1.8,
+			name: 'Magic Helmet',
+			attack: ,
+		},
+		{
+			id: 2.2.1,
+			name: 'Boots',
+			attack: ,
+		},
+		{
+			id: 2.2.2,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.2.3,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.2.8,
+			name: 'Magic Boots',
+			attack: ,
+		},
+		{
+			id: 2.3.1,
+			name: 'Legs',
+			attack: ,
+		},
+		{
+			id: 2.3.2,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.3.3,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.3.8,
+			name: 'Magic Legs',
+			attack: ,
+		},
+		{
+			id: 2.4.1,
 			name: 'Plate Armour',
 			armour: 9,
 		},
 		{
-			id: 5,
+			id: 2.4.2,
 			name: 'Gold Armour',
 			armour: 14,
 		},
 		{
-			id: 6,
+			id: 2.4.3,
+			name: '',
+			attack: ,
+		},
+		{
+			id: 2.4.8,
 			name: 'Magic Plate Armour',
 			armour: 17,
 		},
 	],
 	trinkets: [
 		{
-			id: 7,
+			id: 3.7.1,
 			name: 'Glacier Amulet',
 		},
 		{
-			id: 8,
+			id: 3.8.1,
 			name: 'Garlic Necklace',
 		},
 		{
-			id: 9,
+			id: 3.7.2,
 			name: 'Platinum Amulet',
 		},
 	],
@@ -51,6 +146,11 @@ const EQUIPMENT_TYPES = {
 
 
 export default EQUIPMENT_TYPES;
+/*{
+	id: ,
+	name: '',
+	attack: ,
+},*/
 
 // Carlin Sword
 // Dagger
