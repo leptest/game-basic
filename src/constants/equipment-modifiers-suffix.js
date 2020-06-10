509c4538e07ee63da9bad10c4923bccdb01f20f6
Shinganova <adamchapman13@hotmail.com>
2020-06-10 21:02:20 +0100

    item changes

---
+++ src/constants/equipment-modifiers-suffix.js
@@ -0,0 +1,116 @@
+const EQUIPMENT_MODIFIERS_SUFFIX = {
+  suffix: [
+
+																			{
+																				suffix: 'Inuit',
+																				id: 100,
+																				type: 'cold',
+																			//	modifier: 'increase',
+																				unit: 'percentage',
+																				scaler: 1,
+																				rangeLower: 1,
+																				rangeUpper: 100,
+																			},
+																			{
+																				suffix: 'Seal',
+																				id: 101,
+																				type: 'cold',
+																				//modifier: 'increase',
+																				unit: 'percentage',
+																				scaler: 1,
+																				rangeLower: 1,
+																				rangeUpper: 100,
+																			},
+                                      {
+                                        suffix: 'Penguin',
+                                        id: 102,
+                                        type: 'cold',
+                                        //modifier: 'increase',
+                                        unit: 'percentage',
+                                        scaler: 1,
+                                        rangeLower: 1,
+                                        rangeUpper: 100,
+                                      },
+                                      {
+                                        suffix: 'Yeti',
+                                        id: 103,
+                                        type: 'cold',
+                                        //modifier: 'increase',
+                                        unit: 'percentage',
+                                        scaler: 1,
+                                        rangeLower: 1,
+                                        rangeUpper: 100,
+                                      },
+                                      {
+                                        suffix: 'Walrus',
+                                        id: 104,
+                                        type: 'cold',
+                                        //modifier: 'increase',
+                                        unit: 'percentage',
+                                        scaler: 1,
+                                        rangeLower: 1,
+                                        rangeUpper: 100,
+                                      },
+                                      {
+                                        suffix: 'Emberling',
+                                        id: 105,
+                                        type: 'fire',
+                                        //modifier: 'increase',
+                                        unit: 'percentage',
+                                        scaler: 1,
+                                        rangeLower: 1,
+                                        rangeUpper: 100,
+                                      },
+                                        {
+                                        suffix: 'Fire',
+                                        id: 106,
+                                        type: 'fire',
+                                        //modifier: 'increase',
+                                        unit: 'percentage',
+                                        scaler: 1,
+                                        rangeLower: 1,
+                                        rangeUpper: 100,
+                                      },
+                                      {
+                                      suffix: 'Ash',
+                                      id: 107,
+                                      type: 'fire',
+                                      //modifier: 'increase',
+                                      unit: 'percentage',
+                                      scaler: 1,
+                                      rangeLower: 1,
+                                      rangeUpper: 100,
+                                    },
+                                    {
+                                    suffix: 'Devil',
+                                    id: 108,
+                                    type: 'fire',
+                                    //modifier: 'increase',
+                                    unit: 'percentage',
+                                    scaler: 1,
+                                    rangeLower: 1,
+                                    rangeUpper: 100,
+                                  },
+                                  {
+                                  suffix: 'Dragon',
+                                  id: 109,
+                                  type: 'fire',
+                                  //modifier: 'increase',
+                                  unit: 'percentage',
+                                  scaler: 1,
+                                  rangeLower: 1,
+                                  rangeUpper: 100,
+                                },
+
+
+
+
+
+        ],
+    }
+
+
+
+
+
+export default EQUIPMENT_MODIFIERS_SUFFIX;
